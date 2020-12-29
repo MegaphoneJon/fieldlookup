d376c4899e07f1f1a3a14aa996d16b74766c7c58
agbud8@zabuntu <jon@megaphonetech.com>
2020-12-29 17:33:36 -0500

    Regenerate DAO

--- fieldlookup.civix.php
+++ fieldlookup.civix.php
@@ -7,9 +7,9 @@
  * extension.
  */
 class CRM_Fieldlookup_ExtensionUtil {
-  const SHORT_NAME = "fieldlookup";
-  const LONG_NAME = "fieldlookup";
-  const CLASS_PREFIX = "CRM_Fieldlookup";
+  const SHORT_NAME = 'fieldlookup';
+  const LONG_NAME = 'fieldlookup';
+  const CLASS_PREFIX = 'CRM_Fieldlookup';
 
   /**
    * Translate a string using the extension's domain.
@@ -193,8 +193,9 @@ function _fieldlookup_civix_civicrm_disable() {
  * @param $op string, the type of operation being performed; 'check' or 'enqueue'
  * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
  *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
+ * @return mixed
+ *   based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *   for 'enqueue', returns void
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
  */
@@ -225,7 +226,7 @@ function _fieldlookup_civix_upgrader() {
  * @param string $dir base dir
  * @param string $pattern , glob pattern, eg "*.txt"
  *
- * @return array(string)
+ * @return array
  */
 function _fieldlookup_civix_find_files($dir, $pattern) {
   if (is_callable(['CRM_Utils_File', 'findFiles'])) {
@@ -244,7 +245,7 @@ function _fieldlookup_civix_find_files($dir, $pattern) {
     if ($dh = opendir($subdir)) {
       while (FALSE !== ($entry = readdir($dh))) {
         $path = $subdir . DIRECTORY_SEPARATOR . $entry;
-        if ($entry{0} == '.') {
+        if ($entry[0] == '.') {
         }
         elseif (is_dir($path)) {
           $todos[] = $path;
@@ -255,6 +256,7 @@ function _fieldlookup_civix_find_files($dir, $pattern) {
   }
   return $result;
 }
+
 /**
  * (Delegated) Implements hook_civicrm_managed().
  *
@@ -362,7 +364,7 @@ function _fieldlookup_civix_civicrm_themes(&$themes) {
  * @link http://php.net/glob
  * @param string $pattern
  *
- * @return array, possibly empty
+ * @return array
  */
 function _fieldlookup_civix_glob($pattern) {
   $result = glob($pattern);
@@ -470,20 +472,17 @@ function _fieldlookup_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NUL
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
  */
-
 function _fieldlookup_civix_civicrm_entityTypes(&$entityTypes) {
-  $entityTypes = array_merge($entityTypes, array (
-    'CRM_Fieldlookup_DAO_FieldLookupGroup' => 
-    array (
+  $entityTypes = array_merge($entityTypes, [
+    'CRM_Fieldlookup_DAO_FieldLookupGroup' => [
       'name' => 'FieldLookupGroup',
       'class' => 'CRM_Fieldlookup_DAO_FieldLookupGroup',
       'table' => 'civicrm_field_lookup_group',
-    ),
-    'CRM_Fieldlookup_DAO_FieldLookup' => 
-    array (
+    ],
+    'CRM_Fieldlookup_DAO_FieldLookup' => [
       'name' => 'FieldLookup',
       'class' => 'CRM_Fieldlookup_DAO_FieldLookup',
       'table' => 'civicrm_field_lookup',
-    ),
-  ));
+    ],
+  ]);
 }
