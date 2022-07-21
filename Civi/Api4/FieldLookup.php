e6ab663e426019b6b9cdb8caabdcda43872dd265
Jon Goldberg <jon@megaphonetech.com>
2022-07-21 16:24:20 -0400

    Add API4

---
+++ Civi/Api4/FieldLookup.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * FieldLookup entity.
+ *
+ * Provided by the Fieldlookup extension.
+ *
+ * @package Civi\Api4
+ */
+class FieldLookup extends Generic\DAOEntity {
+
+}
