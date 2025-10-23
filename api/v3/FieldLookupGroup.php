793d063067666ad6b137b7d426bfa1f0198363c5
colemanw <coleman@civicrm.org>
2025-10-22 20:31:20 -0400

    Replace reference to deprecated exception class
    The api-specific exception classes have been deprecated and will soon be removed from core.
    They are identical to CRM_Core_Exception.

--- api/v3/FieldLookupGroup.php
+++ api/v3/FieldLookupGroup.php
@@ -23,7 +23,7 @@ function _civicrm_api3_field_lookup_group_create_spec(&$spec) {
  *
  * @param array $params
  * @return array API result descriptor
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_field_lookup_group_create($params) {
   return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
@@ -34,7 +34,7 @@ function civicrm_api3_field_lookup_group_create($params) {
  *
  * @param array $params
  * @return array API result descriptor
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_field_lookup_group_delete($params) {
   return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
@@ -45,7 +45,7 @@ function civicrm_api3_field_lookup_group_delete($params) {
  *
  * @param array $params
  * @return array API result descriptor
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_field_lookup_group_get($params) {
   return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
